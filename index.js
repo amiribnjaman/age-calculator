ccf5baed9c1aea193348e909936212b56be1b0c9
amiribnjaman <amiribnjaman@gmail.com>
2022-02-13 15:30:49 +0600

    Js functionalities added and complete site done

---
+++ index.js
@@ -0,0 +1,28 @@
+let btn = document.getElementById('submit-btn')
+
+btn.addEventListener('click', function(){
+    let yearInput = document.getElementById('year-input')
+    let yearText = yearInput.value
+    let birthYear = parseInt(yearText)
+
+    let d = new Date()
+    let currentYear = d.getFullYear()
+
+    let birthYearLength = birthYear + ''
+
+    if(birthYearLength.length > 3){
+        var result = currentYear - birthYear
+
+        let show = document.getElementById('show-result')
+        let showCard = document.getElementById('show-card')
+
+        showCard.style.display = 'block'
+        if(result >= 18){
+            show.innerHTML = `<h5  class="text-center">You are ${result} years old.</h5><h5  class="text-center">And you are adult.</h5>`
+        } else {
+            show.innerHTML = `<h5  class="text-center">You are ${result} years old.</h5  class="text-center"><h5>And you are not an adult.</h5>`
+        }
+        show.className = 'show-style'
+    }
+
+})
